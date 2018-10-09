ee70a996a9c4ecb0e74e5498d0e981e9700cd31e
Elvenkitty <raulvarga37@gmail.com>
2018-10-09 14:34:06 +0100

    Changes with Comments
    Added comments for the www\js\index.js
    
    Disabled JQuery and JQueryMobile

--- www/js/index.js
+++ www/js/index.js
@@ -2,6 +2,7 @@ var paused_count =0;
 var resumed_count = 0;
 var launched_count = 0;
 
+//deviceready event listener - deviceready
 document.addEventListener("deviceready", onDeviceReady, false);
 		
 	
@@ -14,9 +15,14 @@ function updateDisplay() {
 
 // device APIs are available
 //
+    //event listeners and handlers are listed in www\js\index.js
+	//lifecycle event onDeviceReady, resume which bring the app back to the foreground and pause which send the app to the background
+	
     function onDeviceReady() {
 	
+	//resume event listener - foreground
 	document.addEventListener("resume", onResume, false);
+	//pause event listener - background
 	document.addEventListener("pause", onPause, false);
 	
 	launched_count++;
